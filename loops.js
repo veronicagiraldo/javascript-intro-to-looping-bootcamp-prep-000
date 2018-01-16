6476464a5d34c514b0da0214c62ad3d62ed7b5a0
Veronica Giraldo <veronica9893@gmail.com>
2018-01-16 19:30:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,11 +2,12 @@
 
 function forLoop(array) 
   for (let i = 0; i < 25; i++) {
-  }
+  
   if (i===1)
     array.push("I am a strange loop.")
     else (i > 1)
-    array.push()
+    array.push("I am ${i} strange loops.")
     return array
 
+}
 }

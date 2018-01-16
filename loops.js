0b9c7aed48287096cc9e5859050f8c2100725889
Veronica Giraldo <veronica9893@gmail.com>
2018-01-16 19:53:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,5 +19,11 @@ while (n>0){
   return 'done' 
 }
 function doWhileLoop(array){
-  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
 }
